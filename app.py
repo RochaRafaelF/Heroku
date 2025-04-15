e4fe86789593e85ed5ec22da363aeaa58daf9b07
RochaRafaelF <rocha.rafaelfonseca@gmail.com>
2025-04-14 23:54:11 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,5 +8,6 @@ def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))  # Heroku define a variável PORT
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+
